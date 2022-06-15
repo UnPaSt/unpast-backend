23421e0037569549b0086f521ee3ccac456a902f
AndiMajore <andi.majore@googlemail.com>
2022-06-16 01:21:07 +0200

    integrated desmond2

--- src/worker/tasks/background.py
+++ src/worker/tasks/background.py
@@ -1,5 +1,6 @@
 from worker.celery import app
 from celery.utils.log import get_task_logger
+
 logger = get_task_logger(__name__)
 
 
@@ -10,3 +11,12 @@ def test_job():
     time.sleep(10)
     logger.info("Done")
     return {"done": True}
+
+
+@app.task(name='desmond2-run')
+def desmond2_job():
+    logger.info("Started Desmond2 execution")
+    import subprocess
+    #FIXME pathing issue of dependent R script
+    subprocess.run(["python3",
+                    "DESMOND2/run_desmond.py" "--exprs" "data/TCGA_200.exprs_z.tsv" "--basename" "TCGA_200" "-p"" 0.001" "--seed" "800691" "-c" "WGCNA"])
